b113f39e39bb19825ab15c4ceb5660484177f24d
Patrik Bergman <patrik.bergman@transmode.com>
2016-10-04 15:59:05 +0200

    Springboot worked much better, no dependency problems!

--- src/main/java/com/infinera/metro/alarm/MainApp.java
+++ src/main/java/com/infinera/metro/alarm/MainApp.java
@@ -2,11 +2,11 @@ package com.infinera.metro.alarm;
 
 import com.infinera.metro.alarm.acceptancetest.testimplementation.AlarmService_Nodes_Test;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
-
+@SpringBootApplication
 public class MainApp {
-
     public static void main(String[] args) {
         org.junit.runner.JUnitCore.main(AlarmService_Nodes_Test.class.getName());
     }
